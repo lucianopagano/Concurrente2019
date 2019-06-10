ecbde1363be7d43d6a2e3de0a66f354b3ab3f1e7
LUCIANO EDUARDO PAGANO <lucho.pagano@gmail.com>
2019-06-10 16:38:23 -0300

    se implemento ejercicio p5e8 y se creo template para 9

---
+++ Practica 5/8-ocho.adb
@@ -0,0 +1,37 @@
+--En una empresa hay 5 controladores de temperatura y una Central. Cada controlador
+--toma la temperatura del ambiente cada 10 minutos y se la envía a una central para que
+--analice el dato y le indique que hacer. Cuando la central recibe una temperatura que es
+--mayor de 40 grados, detiene a ese controlador durante 1 hora.
+
+TASK TYPE Controlador IS
+END Controlador;
+
+TASK BODY Controlador IS
+	double temperatura;
+	boolean detener = false;
+	LOOP
+		delay("10m");
+		temperatura = CalcularTemperatura();
+		Central.monitorearTemperatura(temperatura,detener);
+
+		IF (detener) THEN
+			delay("1h");
+		END
+	END LOOP;
+END Controlador;
+
+TASK Central IS
+	Entry monitorearTemperatura(in double temperatura, out boolean detener)
+END Central;
+
+TASK BODY Central IS
+	double temperatura;
+	boolean detener = false;
+	LOOP 
+		SELECT monitorearTemperatura(temperatura,detener);
+				detener = temperatura > 40.00;
+		END SELECT;
+	END LOOP;
+END Central;
+var
+array of Controlador [1...5];
